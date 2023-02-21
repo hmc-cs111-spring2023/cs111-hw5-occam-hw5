3125b804d04fad5428c7360bd3d5f6d612dcc686
occamkg <38469088+occamkg@users.noreply.github.com>
2023-02-20 23:10:46 -0800

    Write DFA classes

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,31 @@
 package dfa // leave this line in the file
 
-// TODO: replace this comment with your implementation
+import scala.compiletime.ops.string
+
+case class State(label: String)
+
+case class Transition(from: State, to: State, symbol: Char)
+
+class DFA(val states: Set[State], val transitions: Set[Transition],
+          val start: State, val accept: Set[State]) {
+
+    // start at the starting state
+    def accepts(input: String) = acceptsHelper(input, start)
+
+    def acceptsHelper(input: String, currentState: State): Boolean = {
+        // if there is no more input, check if in an accepting state
+        if input.length == 0 then accept.contains(currentState)
+        else {
+            // change the state based on the head character
+            val nextState = transitions.find(transition =>
+                transition.from == currentState &&
+                transition.symbol == input.head) match {
+                    case Some(transition) => transition.to
+                    case None => throw new IllegalArgumentException
+                }
+
+            // check the rest of the input recursively
+            acceptsHelper(input.tail, nextState)
+        }
+    }
+}
